fbb17d7ff5593a9d6f988ac503f79797c1dae464
Redrica <de.spring@gmail.com>
2019-10-29 23:12:34 +0300

    hasta la victoria siempre

---
+++ home.js
@@ -0,0 +1,11 @@
+'use strict';
+// этот модуль home подключает модуль welcome
+
+let welcome = require('./welcome');
+
+welcome('home');
+
+// работает тут, но не работает в другой сборке
+exports.welcome = welcome;
+// такой вариант работает в другой сборке
+//export {welcome}
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './home',
+    output: {
+        filename: './build.js',
+        library: 'home',
+    }
+};
